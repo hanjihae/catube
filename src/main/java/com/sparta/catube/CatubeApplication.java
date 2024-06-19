092289453dcfec68c46b57f115f2b69d769cbaab
hanjihae <wlgo32@naver.com>
2024-06-19 14:06:34 +0900

    initial commit

---
+++ src/main/java/com/sparta/catube/CatubeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.catube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatubeApplication.class, args);
+    }
+
+}
